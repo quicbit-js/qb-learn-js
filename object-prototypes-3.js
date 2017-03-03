08d072223a564d21797b2a2aa29f462b60d2cf74
mvoss9000 <mvoss9000@yahoo.com>
2017-03-03 01:21:51 -0500

    clarify

--- object-prototypes-3.js
+++ object-prototypes-3.js
@@ -42,9 +42,9 @@ property.
 `)
 
 log(`
-function Moose(prop) { this[prop] = 1 }
-Moose
-`)
+> function Moose(prop) { this[prop] = 1 }
+> proto( Moose )`)
+
 function Moose(prop) { this[prop] = 1 }
 proto(Moose)
 
@@ -54,7 +54,7 @@ It should look familiar - it's the default object __proto__
 we used above.  But in the function, the 'prototype' property is
 not like __proto__ at all.  It has no purpose for the function.
 It is only used by the 'new' operator when creating
-objects.  It\'s pretty weird to have this value on every function
+objects.  It's pretty weird to have this value on every function
 in the off chance that that function will be the target of 'new'
 but there you have it.
 The constructor property is also strange.  It has no real purpose
@@ -62,28 +62,28 @@ but debuggers will look to it to surmise the object type name, so
 it's best to have it populated with a functon with a meaningful
 name.  The constructor defaults to the function that contains it
 but again, it isn't actually used for anything.
-So let\'s change the prototype to a bare-bones object to see how
+So let's change the prototype to a bare-bones object to see how
 it works with the 'new' operator...
-`)
-log(`obj.prototype = {a:1, b:2, __proto__: null}`)
-Moose.prototype = {a:1, b:2, __proto__: null, constructor: Moose}
-proto(Moose)
+
+> obj.prototype = { a: 1, b: 2, __proto__: null, constructor: Moose };
+> proto( Moose );`)
+
+Moose.prototype = { a: 1, b: 2, __proto__: null, constructor: Moose }
+proto( Moose )
 
 log(`
-var moose = new Moose('I AM MOOSE!')
-`)
-var moose = new Moose('I AM MOOSE!')
-proto(moose)
+> proto( new Moose('I AM MOOSE!') )`)
+proto( new Moose('I AM MOOSE!') )
 
 log(`
-... and there we have it, the new operator set the object\'s __proto__ instance
-to the \'prototype\' we set in the function.  It was simply transferred over.
+... and there we have it, the new operator set the object's __proto__ instance
+to the 'prototype' we set in the function.  It was simply transferred over.
 The "I AM MOOSE" property was set during construction on the instance.
 Remember the Moose function?
 
    function Moose(prop) { this[prop] = 1 }
 
-We can see that whatever property (or function...) that we set on \'this\' is
+We can see that whatever property (or function...) that we set on 'this' is
 applied to the new instance, one level above the prototype.
 `)
 
@@ -92,20 +92,36 @@ log(`
 returns an object, then THAT object will become the instance rather than a newly created
 object.  IOW, returning an old object from Moose() will cause new to not create anything
 new, which is again... weird.
-`)
+
+> var shared_animal = { shared_prop: '', constructor: Moose, __proto__: null }
+> function Animal() { return shared_animal }
+> Moose.prototype = new Animal();
+> moose1 = new Moose('I AM MOOSE!');
+> moose2 = new Moose('I AM MOOSE AS WELL!')`);
 
 var shared_animal = { shared_prop: '', constructor: Moose, __proto__: null }
 function Animal() { return shared_animal }
-
 Moose.prototype = new Animal()
 moose1 = new Moose('I AM MOOSE!')
 moose2 = new Moose('I AM MOOSE AS WELL!')
+
+log(`
+> proto(moose1)`)
 proto(moose1)
+log(`
+> proto(moose2)`)
 proto(moose2)
 
+log(`
+> shared_animal.shared_prop = "LET US SHARE STUFF!";
+> proto( moose1 );`)
+
 shared_animal.shared_prop = "LET US SHARE STUFF!"
-log(moose1, moose1.shared_prop)
-log(moose2, moose2.shared_prop)
+proto(moose1)
+
+log(`
+> proto( moose2 );`)
+proto(moose2)
 
 log(`
 Creating inheritance hierarchies with the 'new' operator and function prototypes
